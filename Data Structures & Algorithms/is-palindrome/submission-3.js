f1dcc9b31eb5885b77cb253a472fa4e769518df6
Rohit Reghu <53531536+rohitreghu@users.noreply.github.com>
2026-06-07 14:03:57 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const lowerCaseS = s.toLowerCase();
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            const lAscii = lowerCaseS.charCodeAt(l);
+            const rAscii = lowerCaseS.charCodeAt(r);
+            if (!(lAscii >= 48 && lAscii <= 57) && !(lAscii >= 97 && lAscii <= 122)) {
+                l++;
+                continue;
+            }
+            if (!(rAscii >= 48 && rAscii <= 57) && !(rAscii >= 97 && rAscii <= 122)) {
+                r--;
+                continue;
+            }
+            if(lowerCaseS[l] !== lowerCaseS[r]) {
+                return false
+            } else {
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
